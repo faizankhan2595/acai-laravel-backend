dc499aad46db52efde9be8e604c9b0f28d467ef1
Faizan Khan <faizankhan2595@gmail.com>
2024-12-13 11:47:47 +0530

    New

--- app/Notifications/Channels/CustomFcmChannel.php
+++ app/Notifications/Channels/CustomFcmChannel.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Cache;
 use Google\Client;
+use Illuminate\Support\Facades\DB;
 
 class CustomFcmChannel
 {
