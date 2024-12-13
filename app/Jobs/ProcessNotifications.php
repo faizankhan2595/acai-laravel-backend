8eeaf687ff33d18635ecaeccc9064c4dc5c8a409
Faizan Khan <faizankhan2595@gmail.com>
2024-12-13 12:33:06 +0530

    New

--- app/Jobs/ProcessNotifications.php
+++ app/Jobs/ProcessNotifications.php
@@ -27,44 +27,61 @@ class ProcessNotifications implements ShouldQueue
         $this->requestData = $requestData;
     }
 
-    protected function hasRecentNotification($userId)
+    protected function getUsersWithRecentNotifications($userIds)
     {
         return DB::table('notifications')
-            ->where('notifiable_id', $userId)
             ->where('data', 'LIKE', '%' . $this->notification['title'] . '%')
             ->where('created_at', '>=', Carbon::now()->subDay())
-            ->exists();
+            ->whereIn('notifiable_id', $userIds)
+            ->pluck('notifiable_id')
+            ->toArray();
+    }
+
+    protected function processUserBatch($users, $checkMembership = false, $membershipType = null)
+    {
+        // Get IDs of users in current batch
+        $userIds = $users->pluck('id')->toArray();
+        
+        // Get users who already received notification in this batch
+        $recentNotificationUserIds = $this->getUsersWithRecentNotifications($userIds);
+        
+        foreach ($users as $user) {
+            // Skip if user already received notification
+            if (in_array($user->id, $recentNotificationUserIds)) {
+                continue;
+            }
+
+            // Check membership if required
+            if ($checkMembership) {
+                $userMembership = $user->membership(true);
+                if ($userMembership != $membershipType) {
+                    continue;
+                }
+            }
+
+            $user->notify(new GeneralNotification($this->notification));
+        }
     }
 
     public function handle()
     {
         // Process customer notifications
         if ($this->requestData['purple_customers'] || $this->requestData['gold_customers']) {
-            $query = User::role('user')->whereExists(function($query) {
-                $query->from('device_tokens')
-                      ->whereColumn('user_id', 'users.id');
-            });
+            $query = User::role('user')
+                ->whereExists(function($query) {
+                    $query->from('device_tokens')
+                          ->whereColumn('user_id', 'users.id');
+                });
 
-            $query->chunk(300, function($customers) {
-                foreach ($customers as $customer) {
-                    // Skip if user received same notification in past 24 hours
-                    if ($this->hasRecentNotification($customer->id)) {
-                        continue;
-                    }
-
-                    if ($this->requestData['purple_customers'] && !$this->requestData['gold_customers']) {
-                        if ($customer->membership(true) == 1) {
-                            $customer->notify(new GeneralNotification($this->notification));
-                        }
-                    }
-                    else if (!$this->requestData['purple_customers'] && $this->requestData['gold_customers']) {
-                        if ($customer->membership(true) == 2) {
-                            $customer->notify(new GeneralNotification($this->notification));
-                        }
-                    }
-                    else {
-                        $customer->notify(new GeneralNotification($this->notification));
-                    }
+            $query->chunk(500, function($customers) {
+                if ($this->requestData['purple_customers'] && !$this->requestData['gold_customers']) {
+                    $this->processUserBatch($customers, true, 1);
+                }
+                else if (!$this->requestData['purple_customers'] && $this->requestData['gold_customers']) {
+                    $this->processUserBatch($customers, true, 2);
+                }
+                else {
+                    $this->processUserBatch($customers);
                 }
             });
         }
@@ -72,28 +89,16 @@ class ProcessNotifications implements ShouldQueue
         // Process merchant notifications
         if ($this->requestData['all_merchnats']) {
             User::role('merchant')
-                ->chunk(300, function($merchants) {
-                    foreach ($merchants as $merchant) {
-                        // Skip if merchant received same notification in past 24 hours
-                        if ($this->hasRecentNotification($merchant->id)) {
-                            continue;
-                        }
-                        $merchant->notify(new GeneralNotification($this->notification));
-                    }
+                ->chunk(500, function($merchants) {
+                    $this->processUserBatch($merchants);
                 });
         }
 
         // Process sales person notifications
         if ($this->requestData['all_sales_persons']) {
             User::role('sales_person')
-                ->chunk(300, function($sales_persons) {
-                    foreach ($sales_persons as $sales_person) {
-                        // Skip if sales person received same notification in past 24 hours
-                        if ($this->hasRecentNotification($sales_person->id)) {
-                            continue;
-                        }
-                        $sales_person->notify(new GeneralNotification($this->notification));
-                    }
+                ->chunk(500, function($sales_persons) {
+                    $this->processUserBatch($sales_persons);
                 });
         }
     }
